ce1f8c591cc67129a86660c43c254279b8617762
chidiHenry <emeribe@gmail.com>
2023-07-06 12:07:33 +0100

    working iteration

--- src/Fetch.js
+++ src/Fetch.js
@@ -1,25 +1,35 @@
+import axios from "axios";
 
 
-const Fetch = async (url)=>{
-    const response = await fetch(url);
-    const json = await response.json();
+const Fetch =()=>{
+    let city = document.querySelector('#cityName').value;
+    let key = '9310147d5a2cdb67e5ba708d40021005';
+    
+    const url = `http://api.weatherstack.com/current?access_key=${key}&query=${city}` ;
+    fetch(url, {mode: 'cors'})
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(response){
+    document.querySelector('#observationTime').value = `Observation Time: ${response.current.observation_time}` 
+    document.querySelector('#temperature').value = `Temperature :  ${response.current.temperature} Degrees Celsius`;
+    document.querySelector('#humidity').value = `Humidity: ${response.current.humidity}` ;
+    document.querySelector('#visibility').value =  `Visibility: ${response.current.visibility}` ;
+    document.querySelector('#windSpeed').value = `Wind Speed: ${response.current.wind_speed}` ;
+    document.querySelector('#weatherDescriptions').value = `Weather Descriptions: ${response.current.weather_descriptions[0]}` ;
+    document.querySelector('#weatherImage').src = response.current.weather_icons;
+    
 
-    return json;
+    
+
+    
+
+    }, (error)=>{
+       console.log(error);
+    }) 
+
+
 }
 
-let key = 'fb2dff30ce12b64d55d47ddf1a0a6764'
-let city = 'London';
-
-const url = `http://api.weatherstack.com/current?access_key=${key}&query=${city}` 
-
-try{
-    const data = Fetch(url);
-    console.log(data);
-    
-} catch (error) {
-
-    console.log(error);
-    
-}
 
 export default Fetch;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 import Fetch from "./Fetch";
+
+const submitButton = document.querySelector('#form');
+
+submitButton.onsubmit = function(e){
+e.preventDefault();
+
 Fetch();
+
+}
